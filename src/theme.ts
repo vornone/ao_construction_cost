3d3f1aee2a22defbeaf19a7c460c3c56da7b68d8
Vanna Panhavorn <vornone@users.noreply.github.com>
2025-01-13 17:28:02 +0700

    updated

--- src/theme.ts
+++ src/theme.ts
@@ -2,6 +2,7 @@ import { createTheme } from '@mantine/core';
 
 export const theme = createTheme({
   fontFamily: 'gotham, sans-serif',
+
   fontFamilyMonospace: 'Monaco, Courier, monospace',
   headings: { fontFamily: 'Futura, sans-serif' },
 });
